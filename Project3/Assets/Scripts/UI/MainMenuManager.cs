4b282095b21f710519ec575f4cb83e209e3cc321
Borja <mrborjabtf@gmail.com>
2022-01-12 22:26:09 +0100

    Menu created

---
+++ Project3/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public SceneReference level1, level2, level3;
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(level1);
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(level2);
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(level3);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
