b8efbff480abb2bbbf4fd2d748762679c45a58fd
Borja <mrborjabtf@gmail.com>
2022-02-22 19:16:01 +0100

    First attempt Parallax

--- Project3/Assets/Scripts/ProjectUtils.cs
+++ Project3/Assets/Scripts/ProjectUtils.cs
@@ -9,4 +9,18 @@ public class ProjectUtils : MonoBehaviour
         return (Resources.Load("Tilemaps/Tilemap" + (index + 1), typeof(GameObject)) as GameObject);
     }
 
+
+    // Load Parallax
+    public static Sprite LoadBackground(int index)
+    {
+        return (Resources.Load("Backgrounds/Back/Back" + (index + 1), typeof(Sprite)) as Sprite);
+    }
+    public static Sprite LoadMids(int index)
+    {
+        return (Resources.Load("Backgrounds/Mid/Mid" + (index + 1), typeof(Sprite)) as Sprite);
+    }
+    public static Sprite LoadFronts(int index)
+    {
+        return (Resources.Load("Backgrounds/Front/Front" + (index + 1), typeof(Sprite)) as Sprite);
+    }
 }
---
+++ Project3/Assets/Scripts/World/BackgroundController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public int numberOfBackgrounds, numberOfMids, numberOfFronts;
+
+    private float length, startPos;
+    [SerializeField] private GameObject camera;
+    public float parallaxEffect;
+
+    private float totalHeightAcumulated;
+
+    Sprite[] allBackgrounds;
+    Sprite[] allMids;
+    Sprite[] allFronts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBackgrounds();
+        InitializeVariables();
+        Debug.Log(this.name + ": " + length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = camera.transform.position.y * (1 - parallaxEffect);
+        float dist = camera.transform.position.y * parallaxEffect;
+        transform.position = new Vector3(transform.position.x, startPos + dist, transform.position.z);
+
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length) 
+            startPos -= length;
+    }
+
+    void CalculateHeight()
+    {
+        this.transform.position = Vector3.up * totalHeightAcumulated;
+        this.transform.SetParent(this.transform);
+
+        totalHeightAcumulated += this.GetComponent<Sprite>().border.y;
+    }
+
+    void InitializeVariables()
+    {
+        startPos = this.transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    void LoadBackgrounds()
+    {
+        allBackgrounds = new Sprite[numberOfBackgrounds];
+        allMids = new Sprite[numberOfMids];
+        allFronts = new Sprite[numberOfFronts];
+        for (int i = 0; i < numberOfBackgrounds; i++)
+        {
+            ProjectUtils.LoadTilemap(i);
+            allBackgrounds[i] = ProjectUtils.LoadBackground(i);
+        }
+        for (int i = 0; i < numberOfMids; i++)
+        {
+            ProjectUtils.LoadTilemap(i);
+            allMids[i] = ProjectUtils.LoadBackground(i);
+        }
+        for (int i = 0; i < numberOfFronts; i++)
+        {
+            ProjectUtils.LoadTilemap(i);
+            allFronts[i] = ProjectUtils.LoadBackground(i);
+        }
+    }
+}
