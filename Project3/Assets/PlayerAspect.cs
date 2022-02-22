e63629dbb053e065c6f5e103b3e75d3b069c12d5
Pol <psurrielm@gmail.com>
2022-02-22 16:09:17 +0100

    imroved animation controller

--- Project3/Assets/PlayerAspect.cs
+++ Project3/Assets/PlayerAspect.cs
@@ -8,7 +8,10 @@ public class PlayerAspect : MonoBehaviour
     public Rigidbody2D rb;
 
     public Sprite floor;
+    public Sprite floor2;
     public Sprite jump;
+    public Sprite jump2;
+    public Sprite jump3;
     public Sprite wall;
 
    
@@ -33,6 +36,8 @@ public class PlayerAspect : MonoBehaviour
 
     State state;
 
+    int jumpSelected;
+
     public void SetAnimation(State state)
     {
         this.state = state;
@@ -40,10 +45,36 @@ public class PlayerAspect : MonoBehaviour
         switch (state)
         {
             case State.FLOOR:
-                sr.sprite = floor;
+
+                if(transform.up.y < 0f && lastFrameVelocity.normalized.y < -0.5f)
+                {
+                    sr.sprite = floor2;
+
+                }else
+                {
+                    sr.sprite = floor;
+
+                }
+
+                
                 break;
             case State.JUMP:
-                sr.sprite = jump;
+
+                jumpSelected = Random.Range(0,3);
+
+                switch (jumpSelected)
+                {
+                    case 0:
+                        sr.sprite = jump;
+                        break;
+                    case 1:
+                        sr.sprite = jump2;
+                        transform.up = rb.velocity.normalized;
+                        break;
+                    case 2:
+                        sr.sprite = jump3;
+                        break;
+                }
                 break;
             case State.WALL:
                 sr.sprite = wall;
@@ -53,12 +84,36 @@ public class PlayerAspect : MonoBehaviour
         }
     }
 
+    Vector2 lastFrameVelocity;
+
+    private void LateUpdate()
+    {
+        lastFrameVelocity = rb.velocity;
+    }
+
     private void Update()
     {
-        if(state == State.JUMP)
+        
+
+        if (state == State.JUMP)
         {
-            transform.up = ((Vector2)transform.up + rb.velocity * Time.deltaTime).normalized;
-            sr.flipX = rb.velocity.x > 0f;
+            switch (jumpSelected)
+            {
+                case 0:
+                    transform.up = ((Vector2)transform.up + rb.velocity * Time.deltaTime).normalized;
+                    sr.flipX = rb.velocity.x > 0f;
+                    break;
+                case 1:
+                    float sense = rb.velocity.x > 0f ? -1f : 1f;
+                    transform.Rotate(Vector3.forward * Time.deltaTime * rb.velocity.magnitude * 40f * sense);
+                    sr.flipX = rb.velocity.x > 0f;
+                    break;
+                case 2:
+                    transform.up = ((Vector2)transform.up + rb.velocity * Time.deltaTime * 2f).normalized;
+                    sr.flipX = rb.velocity.x < 0f;
+                    break;
+            }
+
         }else
         {
             transform.right = Vector3.right;
