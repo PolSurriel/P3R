4b4f976f6f3d27ba6db67e74cdf78b9d1aca23c9
Diddylcp <mrborjabtf@gmail.com>
2022-02-23 08:59:41 +0100

    Theorical triple parallax
    Created only in theory a three layers parallax without debugging

--- Project3/Assets/Scripts/World/BackgroundController.cs
+++ Project3/Assets/Scripts/World/BackgroundController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,36 +6,46 @@ public class BackgroundController : MonoBehaviour
 {
     public int numberOfBackgrounds, numberOfMids, numberOfFronts;
 
-    private float length, startPos;
+    private float camLength, prevPosition;
     [SerializeField] private GameObject camera;
-    public float parallaxEffect;
 
-    private float totalHeightAcumulated;
+    [Header("Multiplicadores para el efecto Parallax")]
+    [SerializeField] private float parallaxBackgroundMult;
+    [SerializeField] private float parallaxMidMult;
+    [SerializeField] private float parallaxFrontMult;
 
-    Sprite[] allBackgrounds;
-    Sprite[] allMids;
-    Sprite[] allFronts;
+    [SerializeField] private GameObject backgrounds;
+    [SerializeField] private GameObject mids;
+    [SerializeField] private GameObject fronts;
+
+    [Header("Setear el Sprite de más arriba")]
+    [SerializeField] private GameObject lastBackgroundChanged;
+    [SerializeField] private GameObject lastMidChanged;
+    [SerializeField] private GameObject lastFrontChanged;
+
+    private Sprite[] allBackgroundsSprites;
+    private Sprite[] allMidsSprites;
+    private Sprite[] allFrontsSprites;
     // Start is called before the first frame update
     void Start()
     {
         LoadBackgrounds();
         InitializeVariables();
-        Debug.Log(this.name + ": " + length);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        float temp = camera.transform.position.y * (1 - parallaxEffect);
-        float dist = camera.transform.position.y * parallaxEffect;
-        transform.position = new Vector3(transform.position.x, startPos + dist, transform.position.z);
+    {   
+        float cameraDistance = camera.transform.position.y - prevPosition;
+        prevPosition = camera.transform.position.y;
+        // dist es la distancia que retrocede cada una de las imagenes
+        // dependiendo de cuanto parallax se les haya assignado
+        float distBackground = cameraDistance * parallaxBackgroundMult;
+        float distMid = cameraDistance * parallaxMidMult;
+        float distFront = cameraDistance * parallaxFrontMult;
 
-        if (temp > startPos + length)
-        {
-            startPos += length;
-        }
-        else if (temp < startPos - length) 
-            startPos -= length;
+        MoveParallax(distBackground, distMid, distFront);
+        CheckParallax();
     }
 
     void CalculateHeight()
@@ -48,29 +58,79 @@ public class BackgroundController : MonoBehaviour
 
     void InitializeVariables()
     {
-        startPos = this.transform.position.y;
-        length = GetComponent<SpriteRenderer>().bounds.size.y;
+        camLength = camera.orthographicSize * 2;
     }
 
     void LoadBackgrounds()
     {
-        allBackgrounds = new Sprite[numberOfBackgrounds];
-        allMids = new Sprite[numberOfMids];
-        allFronts = new Sprite[numberOfFronts];
+        allBackgroundsSprites = new Sprite[numberOfBackgrounds];
+        allMidsSprites = new Sprite[numberOfMids];
+        allFrontsSprites = new Sprite[numberOfFronts];
         for (int i = 0; i < numberOfBackgrounds; i++)
         {
             ProjectUtils.LoadTilemap(i);
-            allBackgrounds[i] = ProjectUtils.LoadBackground(i);
+            allBackgroundsSprites[i] = ProjectUtils.LoadBackground(i);
         }
         for (int i = 0; i < numberOfMids; i++)
         {
             ProjectUtils.LoadTilemap(i);
-            allMids[i] = ProjectUtils.LoadBackground(i);
+            allMidsSprites[i] = ProjectUtils.LoadBackground(i);
         }
         for (int i = 0; i < numberOfFronts; i++)
         {
             ProjectUtils.LoadTilemap(i);
-            allFronts[i] = ProjectUtils.LoadBackground(i);
+            allFrontsSprites[i] = ProjectUtils.LoadBackground(i);
+        }
+    }
+
+    // Mueve los Sprites para dar el efecto Parallax
+    void MoveParallax(float _distBackground, float _distMid, float _distFront)
+    {
+        foreach(Transform child in backgrounds)
+        {
+            child.position = new Vector3(child.position.x, child.position.y + _distBackground, child.position.z);
+        }
+        foreach(Transform child in mids)
+        {
+            child.position = new Vector3(child.position.x, child.position.y + _distMid, child.position.z);
+        }
+        foreach(Transform child in fronts)
+        {
+            child.position = new Vector3(child.position.x, child.position.y + _distFront, child.position.z);
+        }
+    }
+
+    void CheckParallax()
+    {
+        foreach(Transform child in backgrounds)
+        {
+            float maxDistance = camera.transform.position.y - camLength - 2* child.GetComponent<Sprite>().border.y;
+            if(child.position.y < maxDistance)
+            {
+                // Cambiar Sprite y recolocarlo
+                child.transform.position = lastBackgroundChanged.transform.position + new Vector3(0,lastBackgroundChanged.GetComponent<Sprite>().border.y / 2 + child.GetComponent<Sprite>().border.y /2,0);
+                lastBackgroundChanged = child;
+            }
+        }
+        foreach(Transform child in mids)
+        {
+            float maxDistance = camera.transform.position.y - camLength - 2* child.GetComponent<Sprite>().border.y;
+            if(child.position.y < maxDistance)
+            {
+                // Cambiar Sprite y recolocarlo
+                child.transform.position = lastMidChanged.transform.position + new Vector3(0,lastBackgroundChanged.GetComponent<Sprite>().border.y / 2 + child.GetComponent<Sprite>().border.y /2,0);
+                lastMidChanged = child;
+            }
+        }
+        foreach(Transform child in fronts)
+        {
+            float maxDistance = camera.transform.position.y - camLength - 2* child.GetComponent<Sprite>().border.y;
+            if(child.position.y < maxDistance)
+            {
+                // Cambiar Sprite y recolocarlo
+                child.transform.position = lastFrontChanged.transform.position + new Vector3(0,lastBackgroundChanged.GetComponent<Sprite>().border.y / 2 + child.GetComponent<Sprite>().border.y /2,0);
+                lastFrontChanged = child;
+            }
         }
     }
 }
