cc51e9a0a7ed5fe3f07c3c2def140d9d42b9fe58
Borja <mrborjabtf@gmail.com>
2022-03-03 01:48:09 +0100

    Save System coded
    Files to save skin information created

---
+++ Project3/Assets/Scripts/DataSaving/MenuData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MenuData
+{
+    public string baseSkin;
+    public string suitSkin;
+    public string accessory1;
+    public string accessory2;
+
+    public MenuData(GameInfo gameInfo)
+    {
+        // Coger las variables de gameInfo y asignarlas a las de MenuData
+    }
+}
---
+++ Project3/Assets/Scripts/DataSaving/SaveSys.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSys
+{
+
+    // Saves data in a bin file
+    public static void SaveMenuData(GameInfo gameInfo)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/saves.bin";    // path where its saved
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        MenuData data = new MenuData(gameInfo);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    // Load data from a bin file
+    public static MenuData LoadMenuData()
+    {
+        string path = Application.persistentDataPath + "/saves.bin";    // path from where its loaded
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            MenuData data = formatter.Deserialize(stream) as MenuData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+}
